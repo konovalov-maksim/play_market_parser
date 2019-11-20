12d07e30ee755766311b366f9cc7daed75b6e95a
Maksim <konovalov.ms93@gmail.com>
2019-11-20 23:59:40 +0300

    Доработан парсинг страницы приложения

--- src/main/java/playMarketParser/appsParser/AppLoader.java
+++ src/main/java/playMarketParser/appsParser/AppLoader.java
@@ -1,6 +1,7 @@
 package playMarketParser.appsParser;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import playMarketParser.App;
 import playMarketParser.Connection;
 
@@ -23,7 +24,7 @@ class AppLoader extends Thread {
     public void run() {
         super.run();
         try {
-            parseApp();
+            parseAppPage();
             onAppLoadingCompleteListener.onAppParsingComplete(app, true);
         } catch (IOException e) {
             e.printStackTrace();
@@ -31,14 +32,43 @@ class AppLoader extends Thread {
         }
     }
 
-    private void parseApp() throws IOException {
+    private void parseAppPage() throws IOException {
         String url = app.getUrl() +
                 (language != null ? "&hl=" + language : "") +
                 (country != null ? "&gl=" + country : "");
         Document doc = Connection.getDocument(url);
         if (doc == null) throw new IOException("Не удалось загрузить страницу результатов поиска");
-        //TODO парсинг документа и заполнение полей сущности app
-        app.setCategory(doc.title()); //test
+        //TODO devWebSite, installsCount, minAge, size
+        //name
+        app.setName(doc.getElementsByTag("h1").first().text());
+        //devName, devId
+        try {
+            Element devLink = doc.select("a[href^=/store/apps/developer].hrTbp.R8zArc").first();
+            app.setDevName(devLink.text());
+            app.setDevId(devLink.attr("href").split("=")[1]);
+        } catch (Exception e) {
+            System.out.println("Не удалось получить параметры разработчика");
+        }
+        //deveEmail
+        try {
+            app.setDevEmail(doc.select("a[href^=mailto:].hrTbp.euBY6b").first().text());
+        } catch (Exception e) {System.out.println("Не удалось получить email");}
+        //ratesCount
+        try {
+            app.setRatesCount(Integer.parseInt(
+                    doc.select("span.AYi5wd.TBRnV").first().text().replaceAll(" ", "")));
+        } catch (Exception e) {System.out.println("Не удалось получить число оценок");}
+        //avgRate
+        try {
+            app.setAvgRate(Double.parseDouble(
+                    doc.select("div[aria-label].BHMmbe").first().text().replaceAll(",", ".")));
+        } catch (Exception e) {System.out.println("Не удалось получить ср. оценку");}
+        //category
+        try {
+            app.setCategory(doc.select("a[itemprop=genre]").get(0).text());
+        } catch (Exception e) {
+            System.out.println("Не удалось получить категорию");
+        }
     }
 
     interface OnAppLoadingCompleteListener {
