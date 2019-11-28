ed560b887efde5b45bbda309ba2f4786291b10c9
M.S.Konovalov <konovalov.ms93@gmail.com>
2019-11-28 18:55:56 +0300

    Поправлен стиль таблиц

--- src/main/java/playMarketParser/gui/customElements/RowNumCellFactory.java
+++ src/main/java/playMarketParser/gui/customElements/RowNumCellFactory.java
@@ -8,13 +8,25 @@ public class RowNumCellFactory<T, E> implements Callback<TableColumn<T, E>, Tabl
 
     @Override
     public TableCell<T, E> call(TableColumn<T, E> column) {
-        return new TableCell<T, E>() {
+        TableCell cell = new TableCell<T, E>() {
             @Override
             protected void updateItem(E item, boolean empty) {
                 super.updateItem(item, empty);
-                setText( !empty ? this.getTableRow().getIndex() + 1 + "" : "");
+                setText(!empty ? this.getTableRow().getIndex() + 1 + "" : "");
             }
         };
+        cell.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                cell.getTableView().getSelectionModel().clearSelection();
+                cell.getTableView().getSelectionModel().selectRange(
+                        cell.getIndex(),
+                        (TableColumn) cell.getTableView().getColumns().get(1),
+                        cell.getIndex(),
+                        (TableColumn) cell.getTableView().getColumns().get(cell.getTableView().getColumns().size() - 1));
+            }
+        });
+        cell.getStyleClass().add("row-num-col-cell");
+        return cell;
     }
 
 }
